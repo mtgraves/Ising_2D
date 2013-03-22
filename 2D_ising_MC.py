fd368ef85816138aba6f1c622cce3284f2532ed0
Max Graves <mtg6193@gmail.com>
2013-03-21 23:22:52 -0400

    committing clean up changes to main driver

--- 2D_ising_MC.py
+++ 2D_ising_MC.py
@@ -5,14 +5,15 @@
 # Last Modified:        21-MAR-2013
 #==============================================================================
 
-import random, math
+import random, math, os
 import numpy as np
 import argparse
 
 #==============================================================================
-# parse cmd line 
-#==============================================================================
 def parseCMD():
+    """
+    Parse the command line.
+    """
     parser = argparse.ArgumentParser(description = 'simulate 2-D Ising model')
     parser.add_argument('--temp', '-T', type=float,
             help='enter temperature in kelvin')
@@ -27,174 +28,188 @@ def parseCMD():
     return parser.parse_args()
 
 #==============================================================================
-# Calculate initial energy and magnetization of system, summing 
-# the products of spins of nearest neighbor lattice sites 
-#==============================================================================
-def calcEnergy(aa,L,J):
+def totalEnergy(aa,L,J):
     """
     Sum products of neighboring spin sites.  Return
-    the energy as well as the macroscopic magnetism.
+    the energy as well as the sum of all of the spins individually.
     """
-    # vertical neighbors
     w, h, totalSpin, spinSum = 0, 0, 0.0, 0.0
     while (h <= L-1):
         totalSpin += aa[h,w]
         if ((h == 0) and (w==0)):
-            print 'left top row'
+            #print 'left top row'
             spinSum += (aa[0,1] + aa[0,-1] + aa[1,0] + aa[-1,0])*aa[0,0]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w += 1
         elif ((h==0) and (w<L-1)):
-            print 'middle top row'
+            #print 'middle top row'
             spinSum += (aa[1,w] + aa[-1,w] + aa[0,w-1] + aa[0,w+1])*aa[0,w]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w += 1
         elif ((h==0) and (w==L-1)):
-            print 'right top row'
+            #print 'right top row'
             spinSum += (aa[1,w] + aa[-1,w] + aa[0,0] + aa[0,w-1])*aa[0,w]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w = 0
             h += 1
         elif ((h<L-1) and (w==0)):
-            print 'left middle'
+            #print 'left middle'
             spinSum += (aa[h+1,w] + aa[h-1,w] + aa[h,1] + aa[h,-1])*aa[h,0]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w += 1
         elif ((h<L-1) and (w<L-1)):
-            print 'middle'
+            #print 'middle'
             spinSum += (aa[h+1,w] + aa[h-1,w] + aa[h,w+1] + aa[h,w-1])*aa[h,w]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w += 1
         elif ((h<L-1) and (w==L-1)):
-            print 'right middle'
+            #print 'right middle'
             spinSum += (aa[h+1,w] + aa[h-1,w] + aa[h,0] + aa[h,w-1])*aa[h,w]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w = 0
             h += 1
         elif ((h==L-1) and (w==0)):
-            print 'left bottom'
+            #print 'left bottom'
             spinSum += (aa[0,w] + aa[h-1,w] + aa[h,1] + aa[h,-1])*aa[h,w]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w += 1
         elif ((h==L-1) and (w<L-1)):
-            print 'middle bottom row'
+            #print 'middle bottom row'
             spinSum += (aa[0,w] + aa[h-1,w] + aa[h,w+1] + aa[h,w-1])*aa[h,w]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             w += 1
         elif ((h==L-1) and (w==L-1)):
-            print 'right bottom'
+            #print 'right bottom'
             spinSum += (aa[0,w] + aa[h-1,w] + aa[h,0] + aa[h,w-1])*aa[h,w]
-            print 'w: ', w, 'h: ', h, 'spinSum: ', spinSum
             break
      
-    # calculate energy of the system
     E = - 0.5 * J * spinSum     # divide by two because of double counting
-    M =  math.fabs(totalSpin / pow(L,2))
-    return E,M
+    return E, totalSpin
+
+# =============================================================================
+def energyChange(aa,L,J,w,h):
+    """
+    Calculate energy change for a single electron flipping.
+    """
+    spinSum = 0.0
+    if ((h == 0) and (w==0)):
+        spinSum += (aa[0,1] + aa[0,-1] + aa[1,0] + aa[-1,0])*aa[0,0]
+    elif ((h==0) and (w<L-1)):
+        spinSum += (aa[1,w] + aa[-1,w] + aa[0,w-1] + aa[0,w+1])*aa[0,w]
+    elif ((h==0) and (w==L-1)):
+        spinSum += (aa[1,w] + aa[-1,w] + aa[0,0] + aa[0,w-1])*aa[0,w]
+    elif ((h<L-1) and (w==0)):
+        spinSum += (aa[h+1,w] + aa[h-1,w] + aa[h,1] + aa[h,-1])*aa[h,0]
+    elif ((h<L-1) and (w<L-1)):
+        spinSum += (aa[h+1,w] + aa[h-1,w] + aa[h,w+1] + aa[h,w-1])*aa[h,w]
+    elif ((h<L-1) and (w==L-1)):
+        spinSum += (aa[h+1,w] + aa[h-1,w] + aa[h,0] + aa[h,w-1])*aa[h,w]
+    elif ((h==L-1) and (w==0)):
+        spinSum += (aa[0,w] + aa[h-1,w] + aa[h,1] + aa[h,-1])*aa[h,w]
+    elif ((h==L-1) and (w<L-1)):
+        spinSum += (aa[0,w] + aa[h-1,w] + aa[h,w+1] + aa[h,w-1])*aa[h,w]
+    elif ((h==L-1) and (w==L-1)):
+        spinSum += (aa[0,w] + aa[h-1,w] + aa[h,0] + aa[h,w-1])*aa[h,w]
+    
+    # simply flipping the sign of the energy gives energy change
+    delE =  J * spinSum     # divide by two because of double counting
+    return delE
 
 # =============================================================================
 # begin main function
 # =============================================================================
 def main():
 
-    # assign variables from cmd line input
+    # assign variables, define constants
     args = parseCMD()
     T,H,L,J,s = args.temp, args.field, args.length, args.exchange, args.sweeps
-
     k_B = 1     # = 1.3806503 * pow(10,-23)
 
     # Create random numpy array of spins as either +/- 1
     latt = (2.0000*np.random.randint(2, size=pow(L,2))-1.0000).reshape(L,L)
-
     print latt
 
-    E,M = calcEnergy(latt, L, J)
-
-    print 'E: ',E
-    print 'M: ',M
+    # calculate initial energy and macroscopic magnetism
+    E, totalSpin = totalEnergy(latt, L, J)
+    print 'initial total spin: ',totalSpin
+    M = np.fabs(1.0*totalSpin/(1.0*pow(L,2)))
     
-    # Perform Monte Carlo Sweeps and use Metropolis algorithm
+    # define arrays for mcSteps, Energies, Magnetism
+    mcSteps, Es, Ms = np.arange(s), np.array([E]), np.array([M])
 
-    # define MC step number to run over (THIS NEEDS TO BE SHIFTED)
-    #a = 0
-    # create array to fill MC sweep number and total energy of system
-    #arr_a = np.array([a])
-    #arr_E = np.array([E])
-    #arr_M = np.array([M])
-    #arr_a = np.append(arr_a, a)
-    #arr_E = np.append(arr_E, E)
-    #arr_M = np.append(arr_M, M)
-    #print 'array for a: ', arr_a, 'array for E: ', arr_E
-    #print 'first generated: '
-    #print aa
+    # keep track of acceptance
+    a = 0       # accepted moves
+    r = 0       # rejected moves
 
-    #while (a < s):
-    #    # change the spin of one electron randomly chosen
-    #    # 'r' tells you how many columns to count over
-    #    r = random.randint(0,L)
-    #    # 'c' tells you how many rows to count down 
-    #    c = random.randint(0,L)
-    #    #print 'r:', r, 'c:', c
-    #    if (aa[c,r] == 1):
-    #        aa[c,r] = -1
-    #    else:
-    #        aa[c,r] = 1
-    #    #print 'before sampling: '
-    #    #print aa 
-    #    #E_f = -J*aa[c,r]*(aa[c-1,r] + aa[c+1,r] + aa[c,r-1] + aa[c,r+1])
-    #    Boltz = math.exp(-E_f / (k_B * T))
-    #    if (E_f < 0):
-    #        aa = aa
-    #        Reject = 0
-    #        #print 'accepted because negative'
-    #        #print 'after sampling: '
-    #        #print aa
-    #    elif (E_f == 0):
-    #        aa = aa
-    #        Reject = 0
-    #        #print 'accepted because zero, no change in energy'
-    #        #print 'after sampling: '
-    #        #print aa
-    #    else:
-    #        #generate number between 0 and 1, compare to Boltzmann factor
-    #        n = random.random()
-    #        #print 'random number: ', n
-    #        if (n <= Boltz):
-    #            aa = aa
-    #            Reject = 0
-    #            #print 'accepted by Boltz: ', Boltz
-    #            #print 'after sampling: '
-    #            #print aa
-    #        else:
-    #            if (aa[c,r] == 1):
-    #                aa[c,r] = -1
-    #            else:
-    #                aa[c,r] = 1
-    #            Reject = 1
-    #            #print 'rejected by Boltz: ', Boltz
-    #            #print 'after sampling: '
-    #            #print aa
-    #
-    #
-    #print 'neighbors: ', 'bottom: ', aa[c-1,r], 'top: ', aa[c+1,r]
-    #    #print 'neighbors: ', 'right: ', aa[c, r+1], 'left: ', aa[c,r-1]
-    #    #print 'flip E: ', E_f
-    #
-    #    # recalculate energy and magnetization of spin system  
-    #    # sum products of vertical neighboring spin sites of lattice
-    #    if (Reject == 0):
+    for step in mcSteps:
+        # change the spin of one electron randomly
+        over, down = random.randint(0,L-1), random.randint(0,L-1)
+        #print 'over:', over, 'down:', down
+
+        # check change in energy
+        delE = energyChange(latt, L, J, over, down)
+        #print 'delE: ',delE
+
+        # calculate Boltzmann factor
+        Boltz = np.exp(-1.0*delE/(k_B*T))
+        #print 'Boltz: ',Boltz
+
+        if (delE <= 0):
+            reject = False
+            E += delE
+            #print 'accepted move unconditionally'
+            if (latt[down,over]==1):
+                latt[down,over] = -1
+                totalSpin -= 2
+            else:
+                latt[down,over] = 1
+                totalSpin += 2
+        else:
+            n = random.random()
+            #print 'generated check num: ',n
+            if (n <= Boltz):
+                #print 'accepted move stochastically'
+                E += delE
+                reject = False
+                if (latt[down,over]==1):
+                    latt[down,over] = -1
+                    totalSpin -= 2
+                else:
+                    latt[down,over] = 1
+                    totalSpin += 2
+            else:
+                #print 'rejected move'
+                reject = True
+
+        if reject==True:
+            r += 1
+        else:
+            a += 1
+
+        #print 'new lattice:'
+        #print latt
+
+        # calculate magnetism
+        M = np.fabs(1.0*totalSpin/(1.0*pow(L,2)))
+        Es = np.append(Es, E)
+        Ms = np.append(Ms, M)
+        #print ''
+        #print '==========================================='
     
-    #    else:
-    #        continue
-    #        #print 'ENERGY HAS BEEN LEFT THE SAME: ', E
-    #        #print 'MAGNETIZATION WAS LEFT SAME:   ', M
-    #    a += 1
-    #    arr_a = np.append(arr_a , a)
-    #    arr_E = np.append(arr_E , E)
-    #    arr_M = np.append(arr_M , M)
-    #
-    # output results to a file
-    #filename = "isingOUT.dat"
+    print 'acceptance ratio: ', 1.0*a/(r+a)
+
+    print 'final energy: ', Es[-1]
+    print 'final magnetism: ', Ms[-1]
+    print totalSpin
+    print 'final lattice: '
+    print latt
+
+    if os.path.exists('./data/'):
+        os.chdir('./data/')
+    else:
+        os.mkdir('./data/')
+        os.chdir('./data/')
+
+    filename = 'ising2D_L%s_s%s_Temp%s.dat'%(int(L), s, T)
+    fid = open(filename, 'w')
+    fid.write('# %15s\t%15s\t%15s\n'%('mcSteps','Energies','Magnetism'))
+    zipped = zip(mcSteps, Es, Ms)
+    np.savetxt(fid, zipped, fmt='%5.9f\t%5.9f\t%5.9f')
+    fid.close()
+    print 'Yield data has been saved as ',filename
+ 
     #File = open(filename, "w")
     #File.writelines(arr_a)
     #File.close()
